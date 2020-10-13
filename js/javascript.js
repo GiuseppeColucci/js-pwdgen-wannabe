78c347c34f563f526c3d991e3628a8222eb87f3a
giuseppecolucci <giuseppecolucciwork@gmail.com>
2020-10-13 15:33:59 +0200

    ritocco

--- js/javascript.js
+++ js/javascript.js
@@ -1,10 +1,10 @@
 var nome = prompt('Come ti chiami');
-console.log('nome');
+
 var cognome = prompt('il tuo cognome');
-console.log('cognome');
+
 
 var colore= prompt('quale è il tuo colore preferito');
-console.log('ciao', nome, + cognome);
 
-document.getElementById('benvenuto').innerHTML= 'Benvenuto '+ nome + cognome;
+
+document.getElementById('benvenuto').innerHTML= 'Benvenuto '+ nome + cognome + colore + '19';
 
